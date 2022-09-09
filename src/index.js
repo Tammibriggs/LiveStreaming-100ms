d64f073507c3c69824a59a72b762d91712a58649
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-09-10 00:43:20 +0100

    initialized 100ms

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HMSRoomProvider } from '@100mslive/react-sdk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HMSRoomProvider>
+      <App />
+    </HMSRoomProvider>
   </React.StrictMode>
 );
