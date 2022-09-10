2ce72d2f2c51694651427c322729d41f19c98502
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-09-10 01:52:49 +0100

    Added room/stream control functionalities

--- src/Controls.js
+++ src/Controls.js
@@ -9,11 +9,15 @@ import {
 } from '@mui/icons-material';
 import { IconButton, Button } from '@mui/material';
 import { selectHLSState, useHMSActions, useHMSStore } from '@100mslive/react-sdk'
+import { selectIsLocalAudioEnabled, selectIsLocalVideoEnabled, selectLocalPeer } from '@100mslive/react-sdk'
 
 function Controls() {
 
   const hmsActions = useHMSActions()
   const hlsState = useHMSStore(selectHLSState)
+  const audioEnabled = useHMSStore(selectIsLocalAudioEnabled)
+  const videoEnabled = useHMSStore(selectIsLocalVideoEnabled)
+  const localPeer = useHMSStore(selectLocalPeer)
 
   const startHLSStreaming = async () => {
     try {
@@ -31,41 +35,74 @@ function Controls() {
     }
   }
 
+  const toggleAudio = async () => {
+    await hmsActions.setLocalAudioEnabled(!audioEnabled);
+  }
+
+  const toggleVideo = async () => {
+    await hmsActions.setLocalVideoEnabled(!videoEnabled);
+  }
+
+
+  const leaveRoom = async () => {
+    if(localPeer.roleName === 'broadcaster'){
+      hmsActions.leave()
+      await hmsActions.stopHLSStreaming()
+    }else{
+      hmsActions.leave()
+    }
+  }
+
+  
   return (
     <div className='controls'>
-      <IconButton>
-        <MicOffOutlined />
-      </IconButton>
-
-      <IconButton>
-        <VideocamOffOutlined />
-      </IconButton>
-
-      <Button 
-        variant="contained" 
-        disableElevation
-        className='leave'
-      >
-        <LogoutOutlined /> Leave Room
-      </Button>
-
-      {/* HLS stream button */}
-
-      {hlsState.running
-        ? <Button 
+      {localPeer.roleName === 'broadcaster'
+        ? <>
+            <IconButton onClick={toggleAudio}>
+              {audioEnabled
+                ? <MicNoneOutlined />
+                : <MicOffOutlined />
+              }
+            </IconButton>
+            <IconButton onClick={toggleVideo}>
+              {videoEnabled
+                ? <VideocamOutlined />
+                : <VideocamOffOutlined />
+              }
+            </IconButton>
+            <Button 
+              variant="contained" 
+              disableElevation
+              className='leave'
+              onClick={leaveRoom}
+            >
+              <LogoutOutlined /> Leave Room
+            </Button>
+            {hlsState.running
+              ? <Button 
+                  variant="contained" 
+                  disableElevation
+                  className='leave'
+                  onClick={stopHLSStreaming}
+                >
+                  <StopCircleOutlined /> Stop Streaming
+                </Button>
+              : <Button 
+                  variant="contained"
+                  disableElevation
+                  onClick={startHLSStreaming}
+                >
+                  <PodcastsOutlined /> Go Live 
+                </Button>
+            }
+          </>
+        : <Button 
             variant="contained" 
             disableElevation
             className='leave'
-            onClick={stopHLSStreaming}
+            onClick={leaveRoom}
           >
-            <StopCircleOutlined /> Stop Streaming
-          </Button>
-        : <Button 
-            variant="contained"
-            disableElevation
-            onClick={startHLSStreaming}
-          >
-            <PodcastsOutlined /> Go Live 
+            <LogoutOutlined /> Leave Room
           </Button>
       }
     </div>
