55a96dbb4771730ce08c5c5c6dec2e7fc6201395
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-09-10 01:23:47 +0100

    display video tiles to only broadcasters

--- src/Stream.js
+++ src/Stream.js
@@ -1,11 +1,19 @@
 import VideoTile from "./VideoTile";
 import HlsView from "./HlsView";
+import { selectLocalPeer, selectPeers, useHMSStore } from "@100mslive/react-sdk";
 
 function Stream() {
+  const peers = useHMSStore(selectPeers)
+  const localPeer = useHMSStore(selectLocalPeer)
  
   return (
     <div className="stream">
-      <VideoTile />
+      {localPeer.roleName === 'broadcaster' &&
+        peers.filter((peer) => peer.roleName === 'broadcaster')
+        .map((peer) => (
+          <VideoTile key={peer.id} peer={peer} peers={peers} />
+        )) 
+      }
     </div>
   )
 }
--- src/VideoTile.js
+++ src/VideoTile.js
@@ -1,12 +1,26 @@
-function VideoTile({ peer, peers }) {
+import { useVideo } from "@100mslive/react-sdk";
 
-    return (
-      <video
-        autoPlay
-        muted
-        playsInline
-      />
-    )
+function VideoTile({ peer, peers }) {
+  const { videoRef } = useVideo({
+    trackId: peer.videoTrack
+  })
+
+  const numberOfBroadCasters = () => {
+    const broadcasters = peers.filter((peer) => {
+      return peer.roleName === 'broadcaster'
+    })
+    return broadcasters.length
+  }
+
+  return (
+    <video
+      ref={videoRef}
+      className={numberOfBroadCasters() >= 2 ? 'video' : ''}
+      autoPlay
+      muted
+      playsInline
+    />
+  )
 }
 
 export default VideoTile
